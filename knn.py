a47aa2273566e325b65ca3813b069f1c7f3577b0
Pratiksha <35741003+pseudoGal@users.noreply.github.com>
2018-04-23 10:10:54 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,12 +1,18 @@
+# KNN Classification Algorithm of Supervised Learning
+
+
 #load irisdata
 from sklearn.datasets import load_iris
 iris = load_iris()
+
 #store x & y
 X = iris.data
 y = iris.target
+
 #split data into test & train
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4,random_state = 1 )
+
 #training model on train dataset
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors=3)
@@ -28,6 +34,3 @@ print("Predictions:", pred_species)
 from sklearn.externals import joblib
 joblib.dump(knn, 'iris_knn.pkl')
 
-#output
-#kNN model accuracy: 0.983333333333
-#Predictions: ['versicolor', 'virginica']
